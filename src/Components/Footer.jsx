a86973b279956c8dae195c39fed743f71ff1bb83
jboente <153482214+jboente@users.noreply.github.com>
2024-10-22 10:22:42 +0800

    updated app

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Footer = () => {
+        return (
+    <>
+    <footer>
+        <h2>Contact Details:</h2>
+        <p>Phone: 5555555555</p>
+        <p>Email: planning@events.org</p>
+        <p>&copy; Event Planner Organization. All rights reserved.</p>
+    </footer>
+    </>
+)}
+
+export default Footer
